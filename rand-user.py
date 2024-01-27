770bb02116e53c83fbfc3e860e2463896c045517
Moffi-bit <lorenzostiavelli6@gmail.com>
2024-01-26 22:42:53 -0500

    csv data

--- rand-user.py
+++ rand-user.py
@@ -33,6 +33,7 @@ def organize_requests(resps, path):
     phones = ""
     dates = ""
     coordinates = ""
+    csv_lines = ""
 
     for content in resps:
         content = content["results"][0]
@@ -42,54 +43,82 @@ def organize_requests(resps, path):
         login = content["login"]
         coord = location["coordinates"] 
 
-        names = add_data_to_list(names, name["first"] + " " + name["last"])
-        streets = add_data_to_list(streets, str(street["number"]) + " " + street["name"])
-        cities = add_data_to_list(cities, location["city"])
-        states = add_data_to_list(states, location["state"])
-        countries = add_data_to_list(countries, location["country"])
-        emails = add_data_to_list(emails,  content["email"])
-        usernames = add_data_to_list(usernames, login["username"])
-        passwords = add_data_to_list(passwords, login["password"])
-        phones = add_data_to_list(phones, content["phone"])
-        dates = add_data_to_list(dates, content["registered"]["date"])
-        coordinates = add_data_to_list(coordinates, coord["latitude"] + ", " + coord["longitude"])
+        name_content = name["first"] + " " + name["last"]
+        street_content = str(street["number"]) + " " + street["name"]
+        city_content = location["city"]
+        state_content = location["state"]
+        country_content = location["country"]
+        email_content = content["email"]
+        username_content = login["username"]
+        password_content = login["password"]
+        phone_content = content["phone"]
+        date_content = content["registered"]["date"]
+        coord_content = coord["latitude"] + ", " + coord["longitude"]
 
-    write_data_txt(names, path + "names")
-    write_data_txt(streets, path + "streets")
-    write_data_txt(cities, path + "cities")
-    write_data_txt(states, path + "states")
-    write_data_txt(countries, path + "countries")
-    write_data_txt(emails, path + "emails")
-    write_data_txt(usernames, path + "usernames")
-    write_data_txt(passwords, path + "passwords")
-    write_data_txt(phones, path + "phones")
-    write_data_txt(dates, path + "dates")
-    write_data_txt(coordinates, path + "coordinates")
+        names = handle_data(names, name_content)
+        streets = handle_data(streets, street_content)
+        cities = handle_data(cities, city_content)
+        states = handle_data(states, state_content)
+        countries = handle_data(countries, country_content)
+        emails = handle_data(emails, email_content)
+        usernames = handle_data(usernames, username_content)
+        passwords = handle_data(passwords, password_content)
+        phones = handle_data(phones, phone_content)
+        dates = handle_data(dates, date_content)
+        coordinates = handle_data(coordinates, coord_content)
 
-def write_data_txt(data, path):
-    with open(path + ".txt", "a", encoding="utf-8") as f:
+        csv_lines += "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(name_content, street_content, city_content, state_content, country_content, email_content, username_content, password_content, phone_content, date_content, coord_content)
+
+    write_data(names, path + "names")
+    write_data(streets, path + "streets")
+    write_data(cities, path + "cities")
+    write_data(states, path + "states")
+    write_data(countries, path + "countries")
+    write_data(emails, path + "emails")
+    write_data(usernames, path + "usernames")
+    write_data(passwords, path + "passwords")
+    write_data(phones, path + "phones")
+    write_data(dates, path + "dates")
+    write_data(coordinates, path + "coordinates")
+
+    global ending
+    ending = ".csv"
+
+    path = "./csvs/people/"
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    write_data(csv_lines, path + "people")
+
+def write_data(data, path):
+    with open(path + ending, "a", encoding="utf-8") as f:
         f.write(data)
 
-def add_data_to_list(data, new_data):
+def handle_data(data, new_data):
     if new_data not in data:
         data += new_data + "\n"
     
     return data
 
 def main():
-    ROOT = "./txts/people/"
+    txt = "./txts/people/"
 
     if not os.path.exists("./txts/"):
         os.mkdir("./txts/")
 
-    if not os.path.exists(ROOT):
-        os.mkdir(ROOT)
+    if not os.path.exists ("./csvs/"):
+        os.mkdir("./csvs/") 
+
+    if not os.path.exists(txt):
+        os.mkdir(txt)
 
     resps = send_requests()
-    organize_requests(resps, ROOT)
+    organize_requests(resps, txt)
 
     print("Done!")
 
 
 if __name__ == "__main__":
+    ending = ".txt"
     main()
